99cf0bf8de41fb2008a02fafdb23d64097a19613
AishwaryaLPatil <aishwaryalp166@gmail.com>
2024-03-07 23:52:13 +0530

    Creating basic pipeline scripts

---
+++ main.cpp
@@ -0,0 +1,12 @@
+//File: main.cpp
+#include <iostream>
+using namespace std;
+
+int main(void) {
+  cout << "Hello, Jenkins Pipeline\n";
+  cout << "I have successfully built and run\n";
+  cout << "Jenkins Pipeline is a combination of plugins that support the integration and implementation of continuous delivery pipelines using Jenkins.\n";
+  cout << "My SRN is PES1UG21CS052\n";
+  cout << "My name is Aishwarya L Patil\n";
+  return 0;
+}
